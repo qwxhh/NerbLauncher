64cd35ceb896d07303dd0d82a1cea0513d40dfae
qwxhh <774619917@qq.com>
2017-09-15 23:37:11 +0800

    NerbLauncher

--- app/src/main/java/com/bignerdranch/android/nerdlanucher/NerdLauncherActivity.java
+++ app/src/main/java/com/bignerdranch/android/nerdlanucher/NerdLauncherActivity.java
@@ -9,6 +9,7 @@ public class NerdLauncherActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return NerdLauncherFragment.newInstance();
+        //rghuierthgbi
     }
 
 }
