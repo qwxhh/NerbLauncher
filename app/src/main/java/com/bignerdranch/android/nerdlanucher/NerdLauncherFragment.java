9d03351a6c07dd170a4985c5b9c1d7ca1f364db3
qwxhh <774619917@qq.com>
2017-09-16 15:45:10 +0800

    NerbLauncher

--- app/src/main/java/com/bignerdranch/android/nerdlanucher/NerdLauncherFragment.java
+++ app/src/main/java/com/bignerdranch/android/nerdlanucher/NerdLauncherFragment.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.nerdlanucher;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -55,19 +57,58 @@ public class NerdLauncherFragment extends Fragment{
             }
         });
         Log.i(TAG,"Found"+activities.size()+"activities.");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
     }
-    private class ActivityHolder extends RecyclerView.ViewHolder{
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
         public ActivityHolder(View itemView) {
             super(itemView);
             mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
         }
         public void bindActivity(ResolveInfo resolveInfo){
             mResolveInfo = resolveInfo;
             PackageManager pm = getActivity().getPackageManager();
             String appName = mResolveInfo.loadLabel(pm).toString();
-            mNameTextView.setText(appName);//bhdthbnt
+            mNameTextView.setText(appName);
+        }
+
+        @Override
+        public void onClick(View view) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Intent i =new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName,activityInfo.name)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//为intent添加新任务标志
+            startActivity(i);
+        }
+    }
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder>{
+
+        private final List<ResolveInfo> mActivities;
+
+        private ActivityAdapter(List<ResolveInfo> mActivities) {
+            this.mActivities = mActivities;
+        }
+
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
         }
     }
 }
